cff085fd65f641f671026079d707d62086979c62
Evan Sia <evan.sia@alphasights.com>
2020-10-30 00:37:09 +0000

    Update gradle settings to print out test results

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED
+
 plugins {
     java
     kotlin("jvm") version "1.4.10"
@@ -25,5 +33,52 @@ dependencies {
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>() {
     manifest {
         attributes["Main-Class"] = "org.esia.specialinviter.AppKt"
+    }
+}
+
+tasks.withType<Test> {
+    this.testLogging {
+        // set options for log level LIFECYCLE
+        events = setOf(FAILED, PASSED, SKIPPED, STANDARD_OUT)
+        exceptionFormat = FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+
+        // set options for log level DEBUG and INFO
+        debug {
+            events = setOf(STARTED, FAILED, PASSED, SKIPPED, STANDARD_ERROR, STANDARD_OUT)
+            exceptionFormat = FULL
+        }
+        info.events = debug.events
+        info.exceptionFormat = debug.exceptionFormat
+
+        addTestListener(object : TestListener {
+            override fun beforeTest(p0: TestDescriptor?) = Unit
+            override fun beforeSuite(p0: TestDescriptor?) = Unit
+            override fun afterTest(desc: TestDescriptor, result: TestResult) = Unit
+            override fun afterSuite(desc: TestDescriptor, result: TestResult) {
+                printResults(desc, result)
+            }
+        })
+    }
+}
+
+fun printResults(desc: TestDescriptor, result: TestResult) {
+    if (desc.parent != null) {
+        val output = result.run {
+            "Results: $resultType (" +
+                "$testCount tests, " +
+                "$successfulTestCount successes, " +
+                "$failedTestCount failures, " +
+                "$skippedTestCount skipped" +
+                ") ${desc.className ?: desc.parent}"
+        }
+        val testResultLine = "|  $output  |"
+        val repeatLength = testResultLine.length
+        val seperationLine = "-".repeat(repeatLength)
+        println(seperationLine)
+        println(testResultLine)
+        println(seperationLine)
     }
 }
